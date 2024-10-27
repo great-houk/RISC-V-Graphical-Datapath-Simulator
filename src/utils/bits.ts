54f7de322f0c6f4fac514299168195723a5bf8a9
Tyler Richard <tylerrichard215@gmail.com>
2024-10-27 16:25:45 -0500

    Make truth table work slightly nicer with normal numbers, so enums can be used

--- src/utils/bits.ts
+++ src/utils/bits.ts
@@ -1,5 +1,5 @@
 /** Represents a bit as either a boolean or a 0|1 value. */
-export type Bit = number|boolean
+export type Bit = number | boolean
 
 /**
  * Represents an array of bits as a number[] containing 0s and 1s.
@@ -30,114 +30,118 @@ export function Bits(str: string): Bits
  * @param length The number of bits in the array.
  * @param signed Whether the bits should be signed or not. Defaults to false.
  */
-export function Bits(num: number|bigint, length: number, signed?: boolean): Bits
+export function Bits(num: number | bigint, length: number, signed?: boolean): Bits
 
-export function Bits(src: Bit[]|string|number|bigint, length?: number, signed?: boolean): Bits {
-    if (typeof src == "object") {
-        if (length != undefined || signed != undefined) throw Error("Invalid arguments to Bits()")
-        return fromArray(src)
-    } else if (typeof src == "string") {
-        if (length != undefined || signed != undefined) throw Error("Invalid arguments to Bits()")
-        return fromString(src)
-    } else if (typeof src == "bigint" || typeof src == "number") {
-        if (length == undefined) throw Error("Invalid arguments to Bits()")
-        return fromInt(src, length, signed)
-    } else {
-        throw Error("Invalid arguments to Bits()")
-    }
+export function Bits(src: Bit[] | string | number | bigint, length?: number, signed?: boolean): Bits {
+   if (typeof src == "object") {
+      if (length != undefined || signed != undefined) throw Error("Invalid arguments to Bits()")
+      return fromArray(src)
+   } else if (typeof src == "string") {
+      if (length != undefined || signed != undefined) throw Error("Invalid arguments to Bits()")
+      return fromString(src)
+   } else if (typeof src == "bigint" || typeof src == "number") {
+      if (length == undefined) throw Error("Invalid arguments to Bits()")
+      return fromInt(src, length, signed)
+   } else {
+      throw Error("Invalid arguments to Bits()")
+   }
 }
 
 /** A Template tag which will take a string and return a Bits. */
 export function b(strings: TemplateStringsArray) {
-    return Bits(strings[0])
+   return Bits(strings[0])
 }
 
 // Private methods to create Bits
 function fromArray(arr: Bit[]): Bits {
-    return arr.slice().reverse()
+   return arr.slice().reverse()
 }
 
 function fromString(str: string): Bits {
-    if (!str.match(/^[01 _]*$/)) throw Error(`Invalid bit string "${str}"`)
-    str = str.replace(/[ _]/g, "") // ignore " " and "_"
-    return [...str].map(b => Number(b)).reverse()
+   if (!str.match(/^[01 _]*$/)) throw Error(`Invalid bit string "${str}"`)
+   str = str.replace(/[ _]/g, "") // ignore " " and "_"
+   return [...str].map(b => Number(b)).reverse()
 }
 
-function fromInt(num: number|bigint, length: number, signed: boolean = false): Bits {
-    if (typeof num == "number") num = BigInt(num)
-    let len =  BigInt(length)
-    let [a, b] = signed ? [-(2n**(len-1n)), 2n**(len-1n) - 1n] : [0, 2n**len - 1n]
-    if (num < a || num > b) throw Error(`${num} is invalid. Expected a ${signed ? "" : "un"}signed integer that fits in ${length} bits.`)
+function fromInt(num: number | bigint, length: number, signed: boolean = false): Bits {
+   if (typeof num == "number") num = BigInt(num)
+   let len = BigInt(length)
+   let [a, b] = signed ? [-(2n ** (len - 1n)), 2n ** (len - 1n) - 1n] : [0, 2n ** len - 1n]
+   if (num < a || num > b) throw Error(`${num} is invalid. Expected a ${signed ? "" : "un"}signed integer that fits in ${length} bits.`)
 
-    let bits = Array(length)
-    for (let i = 0; i < length; i++) {
-        bits[i] = Number(num & 0x1n) // Least Significant Bit 0
-        num = num >> 1n
-    }
-    return bits
+   let bits = Array(length)
+   for (let i = 0; i < length; i++) {
+      bits[i] = Number(num & 0x1n) // Least Significant Bit 0
+      num = num >> 1n
+   }
+   return bits
 }
 
 export namespace Bits {
-    /**
-     * Converts a Bits to a bigint
-     * @param bits The bits to convert to an int
-     * @param signed Whether the bits should be interpreted signed or not. Defaults to false.
-     */
-    export function toInt(bits: Bits, signed: boolean = false): bigint {
-        let negative = (signed && bits[bits.length - 1])
+   /**
+    * Converts a Bits to a bigint
+    * @param bits The bits to convert to an int
+    * @param signed Whether the bits should be interpreted signed or not. Defaults to false.
+    */
+   export function toInt(bits: Bits, signed: boolean = false): bigint {
+      let negative = (signed && bits[bits.length - 1])
 
-        if (negative) bits = bits.map(b => +!b) // two's complement, +1 later
+      if (negative) bits = bits.map(b => +!b) // two's complement, +1 later
 
-        let num = 0n
-        for (let i = bits.length - 1; i >= 0; i--) { // Stored backwards, 0 is the index of the LAST bit in binary representation
-            num = num << 1n
-            if (bits[i]) num = num | 0x1n
-        }
+      let num = 0n
+      for (let i = bits.length - 1; i >= 0; i--) { // Stored backwards, 0 is the index of the LAST bit in binary representation
+         num = num << 1n
+         if (bits[i]) num = num | 0x1n
+      }
 
-        if (negative) num = -(num + 1n) // 2's complement. We can't use bitwise ~ here since bigint is already signed.
-        return num
-    }
+      if (negative) num = -(num + 1n) // 2's complement. We can't use bitwise ~ here since bigint is already signed.
+      return num
+   }
 
-    /**
-     * Converts a bits to a number. Note that if the bits is too large precision will be lost. 
-     * @param bits The bits to convert to an number
-     * @param signed Whether the bits should be interpreted signed or not. Defaults to false.
-     */
-    export function toNumber(bits: Bits, signed: boolean = false): number {
-        return Number(Bits.toInt(bits, signed))
-    }
+   /**
+    * Converts a bits to a number. Note that if the bits is too large precision will be lost. 
+    * @param bits The bits to convert to an number
+    * @param signed Whether the bits should be interpreted signed or not. Defaults to false.
+    */
+   export function toNumber(bits: Bits, signed: boolean = false): number {
+      return Number(Bits.toInt(bits, signed))
+   }
 
-    /**
-     * Extends a Bits to the given size.
-     * @param size the size to extend to
-     * @param signed if True, will sign extend
-     */
-     export function extended(bits: Bits, size: number, signed = false): Bits {
-        let sign = signed ? bits[bits.length - 1] : 0 // sign bit
-        let extend = Array(size - bits.length).fill(sign)
-        return bits.concat(extend)
-    }
+   /**
+    * Extends a Bits to the given size.
+    * @param size the size to extend to
+    * @param signed if True, will sign extend
+    */
+   export function extended(bits: Bits, size: number, signed = false): Bits {
+      let sign = signed ? bits[bits.length - 1] : 0 // sign bit
+      let extend = Array(size - bits.length).fill(sign)
+      return bits.concat(extend)
+   }
 
-    /** Converts a bits to a Most Significant Bit 0 order */
-    export function msb0(bits: Bits): Bit[] {
-        return bits.slice().reverse()
-    }
+   /** Converts a bits to a Most Significant Bit 0 order */
+   export function msb0(bits: Bits): Bit[] {
+      return bits.slice().reverse()
+   }
 
-    export function toString(bits: Bits): string {
-        return Bits.msb0(bits).join("")
-    }
+   export function toString(bits: Bits | Number): string {
+      if (bits instanceof Number) {
+         return bits.toString(2)
+      } else {
+         return Bits.msb0(bits).join("")
+      }
+   }
 
-    /** Return true if the two Bits are identical. You can also compare against a string representation of the Bits */
-    export function equal(a: Bits|string, b: Bits|string) {
-        if (typeof a != "string") a = Bits.toString(a)
-        if (typeof b != "string") b = Bits.toString(b)
-        return a == b
-    }
+   /** Return true if the two Bits are identical. You can also compare against a string representation of the Bits */
+   export function equal(a: Bits | string, b: Bits | string) {
+      if (typeof a != "string") a = Bits.toString(a)
+      if (typeof b != "string") b = Bits.toString(b)
+      return a == b
+   }
 
-    /** Concatenates multiple bits */
-    export function join(...arr: (Bits|Bit)[]): Bits {
-        return Bits([]).concat(...arr.reverse())
-    }
+   /** Concatenates multiple bits */
+   export function join(...arr: (Bits | Bit)[]): Bits {
+      return Bits([]).concat(...arr.reverse())
+   }
 }
 
 /**
@@ -145,10 +149,10 @@ export namespace Bits {
  * @param bits The number of bits the int should be. Default 32.
  */
 export function fromTwosComplement(num: bigint, bits: number = 32): bigint {
-    // ~num + 1 doesn't work because of how Javscript does ~
-    let bitsB = BigInt(bits)
-    if (num >= (1n << (bitsB - 1n))) num -= (1n << bitsB) // interpret as two's complement
-    return num
+   // ~num + 1 doesn't work because of how Javscript does ~
+   let bitsB = BigInt(bits)
+   if (num >= (1n << (bitsB - 1n))) num -= (1n << bitsB) // interpret as two's complement
+   return num
 }
 
 /**
@@ -156,6 +160,6 @@ export function fromTwosComplement(num: bigint, bits: number = 32): bigint {
  * @param bits The number of bits the int should be. Default 32.
  */
 export function toTwosComplement(num: bigint, bits: number = 32): bigint {
-    if (num < 0) num += (1n << BigInt(bits))
-    return num
+   if (num < 0) num += (1n << BigInt(bits))
+   return num
 }
--- src/utils/truthTable.ts
+++ src/utils/truthTable.ts
@@ -1,45 +1,54 @@
 import { Bit, Bits } from "./bits"
 
-export type BitDontCares = (Bit|null)[]
+export type BitDontCares = (Bit | null)[]
+export type MatchInput = (Bits | number)[]
+
 export class TruthTable<T> {
-    // TODO Error Checking
-    private table: [BitDontCares[], T][]
+   // TODO Error Checking
+   private table: [BitDontCares[], T][]
 
-    constructor(table: [string[], T][]) {
-        this.table = []
+   constructor(table: [string[], T][]) {
+      this.table = []
 
-        for (let [rowInput, rowOutput] of table) {
-            // Convert to boolean[] with nulls for X
-            let rowInputConv = rowInput.map(str =>
-                [...str].reverse().map(c => c == "X" ? null : Number(c)) // convert to BitDontCares
-            )
-            this.table.push([rowInputConv, rowOutput])
-        }
-    }
+      for (let [rowInput, rowOutput] of table) {
+         // Convert to boolean[] with nulls for X
+         let rowInputConv = rowInput.map(str =>
+            [...str].reverse().map(c => c == "X" ? null : Number(c)) // convert to BitDontCares
+         )
+         this.table.push([rowInputConv, rowOutput])
+      }
+   }
 
-    /**
-     * Takes a bit array, and an array of bits or nulls for don't cares
-     * Example: matchInputs([1, 0, 0], [1, null, 0]) 
-     */
-    private static matchInput(input: Bits, expected: BitDontCares): boolean {
-        if (input.length != expected.length) throw Error("Size mismatch in TruthTable")
-        for (let i = 0; i < input.length; i++) {
-            if (expected[i] != null && expected[i] != input[i]) {
-                return false
-            }
-        }
-        return true
-    }
+   /**
+    * Takes a bit array, and an array of bits or nulls for don't cares
+    * Example: matchInputs([1, 0, 0], [1, null, 0]) 
+    */
+   private static matchInput(input: Bits | number, expected: BitDontCares): boolean {
+      if (input instanceof Bits) {
+         input = input as Bits;
+      }
+      else {
+         input = Bits(input as number, expected.length);
+      }
 
-    /**
-     * Return the proper output for the given inputs to the truth table.
-     */
-    match(...inputs: Bits[]): T {
-        for (let [rowInputs, rowOutputs] of this.table) {
-            if (rowInputs.every( (expected, i) => TruthTable.matchInput(inputs[i], expected) )) {
-                return rowOutputs
-            }
-        }
-        throw Error(`No match for inputs [${inputs.map(b => `"${Bits.toString(b)}"`).join(", ")}] in truth table.`)
-    }
+      if (input.length != expected.length) throw Error("Size mismatch in TruthTable")
+      for (let i = 0; i < input.length; i++) {
+         if (expected[i] != null && expected[i] != input[i]) {
+            return false
+         }
+      }
+      return true
+   }
+
+   /**
+    * Return the proper output for the given inputs to the truth table.
+    */
+   match(...inputs: MatchInput): T {
+      for (let [rowInputs, rowOutputs] of this.table) {
+         if (rowInputs.every((expected, i) => TruthTable.matchInput(inputs[i], expected))) {
+            return rowOutputs
+         }
+      }
+      throw Error(`No match for inputs [${inputs.map(b => `"${Bits.toString(b)}"`).join(", ")}] in truth table.`)
+   }
 }
